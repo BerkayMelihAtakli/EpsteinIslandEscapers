31b871da13b25c9860f9fa9ad92bb79c841d5d77
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-03-11 12:39:30 +0100

    scroll bar style

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Escape Room</title>
-    <link rel="stylesheet" href="/EpsteinIslandEscapers//css/style.css">
+    <link rel="stylesheet" href="/EpsteinIslandEscapers/css/style.css?v=<?php echo filemtime(__DIR__ . '/../css/style.css'); ?>">
 </head>
 
 <body>
